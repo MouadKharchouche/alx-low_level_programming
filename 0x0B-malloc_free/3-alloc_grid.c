6588e51fc573b627f1f3a5cb861ca1013ea51660
MouadKharchouche <mouadkharchouchenational1@gmail.com>
2023-04-11 12:52:46 -0700

    gggg

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,86 +1,74 @@
 #include "main.h"
-		
+
 #include <stdlib.h>
-		
+
+
 /**
-		
- * alloc_grid - nested loop to make grid
-		
- * @width: width input
-		
- * @height: height input
-		
- * Return: pointer to 2 dim. array
-		
+
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+
+ * @width: width of array
+
+ * @height: height of array
+
+ *
+
+ * Return: a pointer to a 2 dimensional array of integers.
+
  */
-		
+
 int **alloc_grid(int width, int height)
-		
+
 {
-		
-	int **mee;
-		
-	int x, y;
-		
 
-		
-	if (width <= 0 || height <= 0)
-		
-		return (NULL);
-		
+        int **array, i, j;
 
-		
-	mee = malloc(sizeof(int *) * height);
-		
+        int len = width * height;
 
-		
-	if (mee == NULL)
-		
-		return (NULL);
-		
 
-		
-	for (x = 0; x < height; x++)
-		
-	{
-		
-		mee[x] = malloc(sizeof(int) * width);
-		
+        if (len <= 0)
 
-		
-		if (mee[x] == NULL)
-		
-		{
-		
-			for (; x >= 0; x--)
-		
-				free(mee[x]);
-		
+                return (NULL);
 
-		
-			free(mee);
-		
-			return (NULL);
-		
-		}
-		
-	}
-		
 
-		
-	for (x = 0; x < height; x++)
-		
-	{
-		
-		for (y = 0; y < width; y++)
-		
-			mee[x][y] = 0;
-		
-	}
-		
+        array = (int **)malloc(sizeof(int *) * height);
 
-		
-	return (mee);
-		
+        if (array == NULL)
+
+                return (NULL);
+
+
+        for (i = 0; i < height; i++)
+
+        {
+
+                array[i] = (int *)malloc(sizeof(int) * width);
+
+                if (array[i] == NULL)
+
+                {
+
+                        for (i--; i >= 0; i--)
+
+                                free(array[i]);
+
+                        free(array);
+
+                        return (NULL);
+
+                }
+
+        }
+
+
+        for (i = 0; i < height; i++)
+
+                for (j = 0; j < width; j++)
+
+                        array[i][j] = 0;
+
+
+        return (array);
+
 }
 
